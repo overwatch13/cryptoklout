04dbafb9a183e3406b58f695ddb4b7f6e3a6f95b
overwatch13 <williamhowley@gmail.com>
2018-03-02 14:13:11 -0500

    small fix for homepage

--- content/root/homepage-content-current-btc.php
+++ content/root/homepage-content-current-btc.php
@@ -16,7 +16,7 @@ if($price['query'] && sizeof($price['query'])>0 && $price['query'][0]['PRICE'] !
   <div class="container text-center">
     <h3>Current Price of Bitcoin is... <span class="text-color-main">$<?php echo $price; ?></span></h3>
     <p>Will the price of BTC be higher or lower tomorrow?</p>
-		<?php if(isset($_SESSION)): ?>
+		<?php if(isset($_SESSION['email'])): ?>
 			<a href="/pages/predictions/prediction-choices.php" class="btn btn-primary navbar-btn btn-shadow btn-gradient" style="margin-top:10px;">Start</a>
 		<?php else: ?>
 			<a href="#" data-toggle="modal" data-target="#modal-signin" class="btn btn-primary navbar-btn btn-shadow btn-gradient" style="margin-top:10px;">Start</a>
